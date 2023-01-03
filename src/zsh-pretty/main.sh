7c4102711d201285efc636f5db8b8f80b8f16048
AppDevMichael <michael.dev012@gmail.com>
2023-01-03 14:12:19 +0000

    Finally fixed copy

--- src/zsh-pretty/main.sh
+++ src/zsh-pretty/main.sh
@@ -8,7 +8,7 @@ USERNAME="${USERNAME:-"automatic"}"
 FEATURE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 curl -L git.io/antigen > ~/antigen.zsh
-cp "${FEATURE_DIR}/scripts/*" "/home/${USERNAME}/"
+cp -r "${FEATURE_DIR}/scripts/.[^.]*" "/home/${USERNAME}"
 
 chown $USERNAME:$USERNAME -R "/home/${USERNAME}/"
 chmod -R 777 "/home/${USERNAME}/"
