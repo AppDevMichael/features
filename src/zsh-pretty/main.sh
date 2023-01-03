0646e3f20aa6232e5c0d33def2dc74fafcea899f
AppDevMichael <michael.dev012@gmail.com>
2023-01-03 13:38:12 +0000

    Cleanup

--- src/zsh-pretty/main.sh
+++ src/zsh-pretty/main.sh
@@ -7,5 +7,10 @@ set -e
 USERNAME="${USERNAME:-"automatic"}"
 FEATURE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
+curl -L git.io/antigen > ~/antigen.zsh
 cp -f "${FEATURE_DIR}/scripts/*" "/home/${USERNAME}/"
+
+chown $USERNAME:$USERNAME -R "/home/${USERNAME}/"
+chmod -R 777 "/home/${USERNAME}/"
+
+echo "Done!"
