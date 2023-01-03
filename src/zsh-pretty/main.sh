76d416323980a83a39880cc5610fc7081b5ac147
AppDevMichael <michael.dev012@gmail.com>
2023-01-03 14:14:39 +0000

    I guess I didn't fix it

--- src/zsh-pretty/main.sh
+++ src/zsh-pretty/main.sh
@@ -8,7 +8,8 @@ USERNAME="${USERNAME:-"automatic"}"
 FEATURE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 curl -L git.io/antigen > ~/antigen.zsh
-cp -r "${FEATURE_DIR}/scripts/.[^.]*" "/home/${USERNAME}"
+cp -r "${FEATURE_DIR}/scripts/.zshrc" "/home/${USERNAME}"
+cp -r "${FEATURE_DIR}/scripts/.p10k.zsh" "/home/${USERNAME}"
 
 chown $USERNAME:$USERNAME -R "/home/${USERNAME}/"
 chmod -R 777 "/home/${USERNAME}/"
